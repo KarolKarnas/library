153371caf690728d8a6f742196f0282288e08048
KarolKarnas <karol.karnas@gmail.com>
2023-01-06 09:43:42 +0100

    Create html user input
    Create HTML user input: title, author, pages, read. Input connected with label element. Add basic styling in css.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-let myLibrary = [];
-
-
+let myLibrary = [
+	{
+		title: 'Dune',
+		author: 'Frank Herbert',
+		pages: 350,
+		read: 'read',
+	},
+	{
+		title: 'A Game of Thrones',
+		author: 'George R. R. Martin',
+		pages: 700,
+		read: 'not read yet',
+	},
+	{
+		title: 'C.S. Lewis',
+		author: 'The Chronicles od Narnia',
+		pages: 900,
+		read: 'not read yet',
+	},
+	{
+		title: '',
+		author: '',
+		pages: 0,
+		read: '',
+	},
+];
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -10,16 +33,14 @@ function Book(title, author, pages, read) {
 	this.info = () => `${title} by ${author}, ${pages}, ${read}`;
 }
 
-
-
 function addBookToLibrary() {
-  // do stuff here
+	// do stuff here
 }
-
-
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
 
 console.log(theHobbit.info());
 
 console.log(theHobbit.constructor);
+
+console.log(theHobbit);
